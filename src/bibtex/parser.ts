5640cbb4509ebd003eb1095c919d09ca66e106e7
Derek P Sifford <dereksifford@gmail.com>
2017-09-11 14:32:28 -0400

    add initial support for bibtex => csl name parsing

--- src/bibtex/parser.ts
+++ src/bibtex/parser.ts
@@ -10,7 +10,7 @@ interface DatePart {
 type PartialDate0 = () => PartialDate1;
 type PartialDate1 = (first?: DatePart) => PartialDate2;
 type PartialDate2 = (second?: DatePart) => Partial<CSL.Data> | undefined;
-type CurriedDate = PartialDate0 | PartialDate1 | PartialDate2 | (Partial<CSL.Data> | undefined);
+type CurriedDate = PartialDate1 | PartialDate2 | (Partial<CSL.Data> | undefined);
 
 const curriedDate: PartialDate0 = () => (first = { value: '' }) => (second = { value: '' }) => {
     if (!first.kind) return;
@@ -19,9 +19,51 @@ const curriedDate: PartialDate0 = () => (first = { value: '' }) => (second = { v
         : { issued: { 'date-parts': [[second.value, first.value, '']] } };
 };
 
-// Disabling below because switch statements will generally increase "complexity", but that's by
-// design. The function really isn't that complex.
-// tslint:disable cyclomatic-complexity
+const parseNames = (input: string): CSL.Person[] => {
+    const FIRST_VON_LAST = /([A-Z][a-zA-Z-]+ )?([a-z][a-zA-Z-]+ .+? [a-z][a-zA-Z-]+ )?([A-Z].+)/;
+    const VON_LAST_FIRST = /(.+ [a-z][a-zA-Z-]+ )?(.+), (.+)/;
+    const VON_LAST_JR_FIRST = /(.+ [a-z][a-zA-Z-]+ )?(.+), (.+), (.+)/;
+    const rawNames = input.split(/ and (?:others)?/g).filter(Boolean);
+    let names: CSL.Person[] = [];
+
+    for (const name of rawNames) {
+        let first = '';
+        let von = '';
+        let last = '';
+        let jr = '';
+        let match: RegExpMatchArray | null;
+        switch (name.split(',').length) {
+            case 1:
+                match = name.match(FIRST_VON_LAST);
+                if (!match) break;
+                [, first, von, last] = match;
+                break;
+            case 2:
+                match = name.match(VON_LAST_FIRST);
+                if (!match) break;
+                [, von, last, first] = match;
+                break;
+            case 3:
+                match = name.match(VON_LAST_JR_FIRST);
+                if (!match) break;
+                [, von, last, jr, first] = match;
+                break;
+            default:
+                throw new Error(`Invalid name format found: ${name}`);
+        }
+        names = [
+            ...names,
+            {
+                ...last ? { family: last } : {},
+                ...first ? { given: first } : {},
+                ...jr ? { suffix: jr } : {},
+                ...von ? { 'dropping-particle': von } : {},
+            },
+        ];
+    }
+    return names;
+};
+
 const parseValue = (value: ValueType | ValueType[], macros: Map<string, string>): string => {
     let output = '';
     const input: ValueType[] = Array.isArray(value) ? value : [value];
@@ -60,25 +102,36 @@ export default function parseCSL(source: string): CSL.Data[] {
                 STRINGS_MAP.set(node.key, parseValue(node.value, STRINGS_MAP));
                 break;
             case 'Entry':
+                // TODO: Extract into own function
                 let entry: CSL.Data = {
                     id: node.id,
                     type: TYPE_MAP.get(node.type) || 'article',
                 };
                 let date: CurriedDate = curriedDate();
                 for (const property of node.properties) {
-                    if (property.key === 'year' || property.key === 'month') {
-                        date = date({
-                            kind: property.key,
-                            value: parseValue(property.value, STRINGS_MAP),
-                        });
-                        continue;
-                    }
-                    const field = FIELD_MAP.get(property.key);
-                    if (field) {
-                        entry = {
-                            ...entry,
-                            [field]: parseValue(property.value, STRINGS_MAP),
-                        };
+                    switch (property.key) {
+                        case 'year':
+                        case 'month':
+                            date = date({
+                                kind: property.key,
+                                value: parseValue(property.value, STRINGS_MAP),
+                            });
+                            break;
+                        case 'author':
+                        case 'editor':
+                            entry = {
+                                ...entry,
+                                [property.key]: parseNames(parseValue(property.value, STRINGS_MAP)),
+                            };
+                            break;
+                        default:
+                            const field = FIELD_MAP.get(property.key);
+                            if (field) {
+                                entry = {
+                                    ...entry,
+                                    [field]: parseValue(property.value, STRINGS_MAP),
+                                };
+                            }
                     }
                 }
                 while (typeof date === 'function') {
